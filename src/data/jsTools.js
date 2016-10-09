2a29f8cc054eca376d27baaad34f8d846a008d2f
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-10-09 21:41:28 +0300

    Add a lot of images + update jsTools list

--- src/data/jsTools.js
+++ src/data/jsTools.js
@@ -1,17 +1,113 @@
 window.jsTools = [
   {
-    name: 'Vue'
+    name: 'Angular2'
   },
   {
     name: 'AngularJS'
   },
   {
-    name: 'Angular2'
+    name: 'Aurelia'
+  },
+  {
+    name: 'Babel'
+  },
+  {
+    name: 'Backbone'
+  },
+  {
+    name: 'Bower'
+  },
+  {
+    name: 'Brocolli'
+  },
+  {
+    name: 'Browserify'
+  },
+  {
+    name: 'Chai'
+  },
+  {
+    name: 'CoffeeScript'
+  },
+  {
+    name: 'CommonJS'
+  },
+  {
+    name: 'D3'
+  },
+  {
+    name: 'Deku'
+  },
+  {
+    name: 'Ember'
+  },
+  {
+    name: 'ES6'
+  },
+  {
+    name: 'ESLint'
+  },
+  {
+    name: 'Grunt'
+  },
+  {
+    name: 'Gulp'
+  },
+  {
+    name: 'Handlebars'
+  },
+  {
+    name: 'jQuery'
+  },
+  {
+    name: 'JSX'
+  },
+  {
+    name: 'KeyStone'
+  },
+  {
+    name: 'Knockout'
+  },
+  {
+    name: 'Meteor'
+  },
+  {
+    name: 'Mithril'
+  },
+  {
+    name: 'Mobx'
+  },
+  {
+    name: 'NodeJS'
+  },
+  {
+    name: 'npm'
+  },
+  {
+    name: 'Polymer'
+  },
+  {
+    name: 'RactiveJS'
+  },
+  {
+    name: 'Ramda'
   },
   {
     name: 'React'
   },
   {
-    name: 'Polymer'
+    name: 'Redux'
+  },
+  {
+    name: 'RiotJS'
+  },
+  {
+    name: 'TypeScript'
+  },
+  {
+    name: 'Vue'
+  },
+  {
+    name: 'Webpack'
   }
 ]
