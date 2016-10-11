9b10f6b16e918df802e9318641df43e289bdf861
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-10-11 20:47:57 +0300

    Make option animation longer

--- src/components/UiOptions.vue
+++ src/components/UiOptions.vue
@@ -44,7 +44,7 @@ export default {
   margin-left: 32px;
 }
 .fade-enter-active, .fade-leave-active {
-  transition: opacity 1.5s
+  transition: opacity 2s
 }
 .fade-enter, .fade-leave-active {
   opacity: 0
