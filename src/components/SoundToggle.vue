b7a097586677ff73703300649e3c83f0476c04de
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-11-07 16:08:06 +0200

    Replace sound icons with svg

--- src/components/SoundToggle.vue
+++ src/components/SoundToggle.vue
@@ -1,5 +1,12 @@
 <template>
-  <a href="#" v-on:click="onClick"><i class="material-icons" v-if="sound">volume_up</i><i class="material-icons" v-else>volume_off</i></a>
+  <div>
+    <svg v-if="sound" v-on:click="onClick">
+      <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path>
+    </svg>
+    <svg v-else v-on:click="onClick">
+      <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"></path>
+    </svg>
+  </div>
 </template>
 
 <script>
@@ -16,12 +23,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
+div {
   position: absolute;
   top: 15px;
   left: 20px;
-  font-size: 14px;
+}
+
+svg {
   color: #66BB6A;
-  text-decoration: none;
+  fill: currentColor;
+  cursor: pointer;
+  width: 30px;
+  height: 30px;
 }
 </style>
