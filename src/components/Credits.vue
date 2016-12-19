cdb2abe280d91c8227f6190762ade5d2a8716f23
Sami Suo-Heikki <samiheikki@vaadin.com>
2016-12-19 14:58:55 +0200

    add icons to credits

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
-    By <a href="https://suoheikki.com">Sami Suo-Heikki</a> and <a href="https://github.com/samiheikki/javascript-guessing-game/graphs/contributors">Contributors</a>
+    By <a href="https://github.com/samiheikki" target="_blank">
+      Sami Suo-Heikki
+      <svg fill="#66BB6A" height="12" viewBox="0 0 24 24" width="12" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
+      </svg>
+    </a>
+    and
+    <a href="https://github.com/samiheikki/javascript-guessing-game/graphs/contributors" target="_blank">
+      Contributors
+      <svg fill="#66BB6A" height="12" viewBox="0 0 24 24" width="12" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
+      </svg>
+    </a>
   </div>
 </template>
 
