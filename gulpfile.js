6b0c1deb94c079126a2553611c26c2434df778b7
Sami Suo-Heikki <samiheikki@vaadin.com>
2017-03-24 21:05:59 +0200

    Hash logo file names in production

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,21 @@ gulp.task('generate-service-worker', function(callback) {
   var rootDir = 'dist';
 
   swPrecache.write(path.join(rootDir, 'sw.js'), {
-    "staticFileGlobs": [rootDir + '/**/*.{html,css,png,ico,js,map,ogg,mp3}'],
-    "stripPrefix": rootDir
+    'staticFileGlobs': [rootDir + '/**/*.{html,css,png,ico,js,map,ogg,mp3}'],
+    'stripPrefix': rootDir
   }, callback);
+});
+
+gulp.task('rename-images', function(callback) {
+  var fs = require('fs');
+  var logos = JSON.parse(fs.readFileSync('./static/logos.json'));
+  logos.forEach((logo) => {
+    const base64 = new Buffer(logo.name).toString('base64');
+    fs.rename('./dist/static/logos/' + logo.name + '.png', './dist/static/logos/' + base64 + '.png', err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+  callback();
 });
