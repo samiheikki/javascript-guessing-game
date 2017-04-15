890051ab881cae34f0e8d3ff66145ea3812ed18f
Sami Suo-Heikki <samiheikki@vaadin.com>
2017-04-15 07:05:05 +0300

    Add promise polyfill to support ie11

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'es6-promise/auto' // Polyfill ie11
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { sync } from 'vuex-router-sync'
