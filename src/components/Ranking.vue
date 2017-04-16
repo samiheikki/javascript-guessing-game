e81a9d3135f81422598417729b6e08a194e0ccf7
Sami Suo-Heikki <samiheikki@vaadin.com>
2017-04-16 08:02:15 +0300

    Style fixes to ranking page

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
-    <router-link to="/">
-      <h4>
-        <svg fill="#FFFFFF" height="24" viewBox="0 0 24 16" width="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
-        </svg>
-        Play the game
-      </h4>
-    </router-link>
-
+    <div class="flex">
+      <router-link to="/">
+        <h4>
+          <svg fill="#FFFFFF" height="24" viewBox="0 0 24 16" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+          </svg>
+          Play the game
+        </h4>
+      </router-link>
+    </div>
     <h1>High Scores (top 10)</h1>
     <div class="scores-container">
       <div class="score-row" v-for="(score, index) in highScores">
@@ -84,14 +85,14 @@ h1, h3 {
   color: #66BB6A;
   word-wrap: break-word;
 }
-
 a {
   text-decoration: none;
 }
-
 h4 {
-  text-align: left;
   color: white;
+}
+.flex {
+  display: flex; 
 }
 
 @media screen and (max-width:620px) {
