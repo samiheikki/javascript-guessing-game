230b0880ca7330e378042ebb448772af5b5098a5
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2018-02-08 08:51:45 +0200

    Fix linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-'use strict';
-var gulp = require('gulp');
-const uuidv4 = require('uuid/v4');
+'use strict'
+var gulp = require('gulp')
+const uuidv4 = require('uuid/v4')
 
-gulp.task('generate-service-worker', function(callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
-  var rootDir = 'dist';
+gulp.task('generate-service-worker', callback => {
+  var path = require('path')
+  var swPrecache = require('sw-precache')
+  var rootDir = 'dist'
 
   swPrecache.write(path.join(rootDir, 'sw.js'), {
     'staticFileGlobs': [rootDir + '/**/*.{html,css,png,ico,js,map,ogg,mp3}'],
     'stripPrefix': rootDir
-  }, callback);
-});
+  }, callback)
+})
 
-gulp.task('rename-images', function(callback) {
-  var fs = require('fs');
-  var logos = JSON.parse(fs.readFileSync('./static/logos.json'));
+gulp.task('rename-images', callback => {
+  var fs = require('fs')
+  var logos = JSON.parse(fs.readFileSync('./static/logos.json'))
   logos.forEach((logo) => {
-    const uuid = uuidv4();
-    fs.renameSync('./dist/static/logos/' + logo.name.toLowerCase() + '.png', './dist/static/logos/' + uuid + '.png');
-    logo.uuid = uuid;
-  });
-  fs.writeFileSync('./dist/static/logos.json', JSON.stringify(logos, null, 2)); 
-  callback();
-});
+    const uuid = uuidv4()
+    fs.renameSync('./dist/static/logos/' + logo.name.toLowerCase() + '.png', './dist/static/logos/' + uuid + '.png')
+    logo.uuid = uuid
+  })
+  fs.writeFileSync('./dist/static/logos.json', JSON.stringify(logos, null, 2))
+  callback()
+})
--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-import * as types from './mutation-types'
-
-// export const logIn = ({ commit }) => {
-//   commit(types.LOG_IN)
-// }
-
-// export const startListeningToAuth = ({ commit }) => {
-//   commit(types.LISTEN_AUTH)
-// }
